643a97342ad5a05db1ba177b45e5aca7931e5666
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-04-21 17:59:34 +0900

    0

---
+++ build.py
@@ -0,0 +1,11 @@
+import platform
+
+os = platform.system()
+if os == "Windows":
+    print(f"OS: {os}")
+elif os == "Linux":
+    print(f"OS: {os}")
+elif os == "Darwin":
+    print(f"OS: {os}")
+else:
+    print(f"OS: {os}")
